6e9e15265d3baa6514228b03a0a36450abe99b09
ender <ender@DESKTOP-6D9BIC0>
2021-07-24 20:00:50 -0300

    Add project files.

---
+++ PROJETOCALCULADORA/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PROJETOCALCULADORA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtOperacao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(55, 91);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(179, 91);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtOperacao
+            // 
+            this.txtOperacao.Location = new System.Drawing.Point(104, 53);
+            this.txtOperacao.MaxLength = 30;
+            this.txtOperacao.Name = "txtOperacao";
+            this.txtOperacao.Size = new System.Drawing.Size(109, 20);
+            this.txtOperacao.TabIndex = 0;
+            this.txtOperacao.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtOperacao_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(95, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escreva o algoritmo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 140);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOperacao);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtOperacao;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PROJETOCALCULADORA/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PROJETOCALCULADORA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int parcela1;
+            int parcela2;
+            string tipo_operacao;
+            int resultado= -111111111;
+
+            string algoritmo = txtOperacao.Text;
+
+            string sinais = "+,-,*,/";
+
+            int index = 0;
+            foreach( char c in algoritmo)
+            {
+                foreach(char s in sinais)
+                {
+                    if (c == s){
+                        //DEFINE OS 3 ELEMENTOS DA OPERACAO
+                        //parcela1!
+                        parcela1 = int.Parse(algoritmo.Substring(0,index));
+                        //SINAL
+                        tipo_operacao = s.ToString();
+                        //PARCELA2
+                        parcela2 = int.Parse(algoritmo.Substring(index + 1));
+                        //ANALISE A OPERACAO QUE VAI SER REALIZADA
+                        switch (tipo_operacao)
+                        {
+                            //ADICAO
+                            case "+":
+                                resultado = parcela1 + parcela2;
+                                break;
+                                //SUBTRACAO
+                            case "-":
+                                resultado = parcela1 - parcela2;
+                                break;
+                                //MULTIPLICACAO
+                            case "*":
+                                resultado = parcela1 * parcela2;
+                                break;
+                                //DIVISAO
+                            case "/":
+                                resultado = parcela1 / parcela2;
+                                break;
+
+                        }
+                        break;
+                            
+                    }
+                }
+                if (resultado != -111111111)
+                    break;
+                index++;
+            }
+            if (resultado == -111111111)
+                MessageBox.Show("Houve um erro, por favor tente novamente");
+            //APRESENTAR RESULTADO FINAL
+            else
+            {
+                MessageBox.Show(algoritmo + " = " + resultado.ToString(),"Resultado", MessageBoxButtons.OK);
+            }
+
+            txtOperacao.Text = "";
+            txtOperacao.Focus();
+
+            
+        }
+
+        private void txtOperacao_KeyDown(object sender, KeyEventArgs e)
+        {
+            //VERIFICA SE TEM TEXTO PRA CALCULAR
+            if (txtOperacao.Text == "") return;
+
+            //SE APERTAR ENTER EXECUTA O CALCULO
+            if (e.KeyCode == Keys.Return)
+                btnCalcular_Click(btnCalcular, EventArgs.Empty);
+
+            else if (e.KeyCode == Keys.Escape)
+            {
+                txtOperacao.Text="";
+                txtOperacao.Focus();
+            }
+
+        }
+    }
+}
